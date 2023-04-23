5a5aa8c62de53e6f01511e9f7ea76bfeddb3158b
Simran-Kaur1996 <simran31kaur@gmail.com>
2023-04-23 14:20:01 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'cards';
+  post = [
+    {
+      title: 'Neat Tree',
+      imageUrl: 'assets/tree.jpeg',
+      username: 'nature',
+      content: 'I saw this neat tree today',
+    },
+    {
+      title: 'Snowy Mountain',
+      imageUrl: 'assets/mountain.jpeg',
+      username: 'mountainlover',
+      content: 'here is picture of a snowy mountain',
+    },
+    {
+      title: 'Mountain biking',
+      imageUrl: 'assets/biking.jpeg',
+      username: 'biking',
+      content: ' I did some biking today',
+    },
+  ];
 }
